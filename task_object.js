56c456527744d953a008d933c5da583a7ca5b656
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-09 20:35:43 +0700

    TASK OBJECT

--- task_object.js
+++ task_object.js
@@ -1,50 +1,50 @@
 //
-const motor ={
+const motor = {
     banDepan: "dunlop",
     banBelakang: "dunlop",
     isSport: false,
-    getObjMotor: function(){
+    getObjMotor: function () {
         console.log(motor.banDepan)
         console.log(motor["banBelakang"])
-        
+
         motor.bensin = "Pertalite"
         motor["cc"] = "125"
         console.log(motor.isSport)
         console.log(motor.bensin)
-        console.log(motor.cc+"\n")
+        console.log(motor.cc + "\n")
     }
 }
 
 //
-const mobil ={
+const mobil = {
     merk: "Honda",
     type: "Hatcback",
     isSport: false,
-    getObjMobil: function(){
+    getObjMobil: function () {
         console.log(mobil.merk)
         console.log(mobil["type"])
-        
+
         mobil.bensin = "Pertamax"
         mobil["cc"] = "1500"
         console.log(mobil.isSport)
         console.log(mobil.bensin)
-        console.log(mobil.cc+"\n")
+        console.log(mobil.cc + "\n")
     }
 }
 
-const laptop ={
+const laptop = {
     merk: "HP",
     type: "Touch Screen",
     isGaming: false,
-    getObjLaptop: function(){
+    getObjLaptop: function () {
         console.log(laptop.merk)
         console.log(laptop["type"])
-        
+
         laptop.proc = "Intel i9"
         laptop["ram"] = "16GB"
         console.log(laptop.isGaming)
         console.log(laptop.proc)
-        console.log(laptop.ram+"\n")
+        console.log(laptop.ram + "\n")
     }
 }
 
